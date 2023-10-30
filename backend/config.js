f1b8419b2c64286763ffd6453da7b02d9e45ec85
AkramDadKhan1 <akram.khan@fyntune.com>
2023-10-30 11:52:19 +0530

    MogoDB URL Changes

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-  "mongodb+srv://root:root@book-store-mern.lbngxdp.mongodb.net/books-collection?retryWrites=true&w=majority";
+  "mongodb+srv://mongoad17:mongoad17@cluster0.qrvvgwi.mongodb.net/?retryWrites=true&w=majority";
